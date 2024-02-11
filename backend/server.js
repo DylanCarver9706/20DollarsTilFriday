b9f87d124aaca1ae6aac9d4044d9cb6c7dfcdd04
Dylan Carver <dylancarver14@gmail.com>
2024-02-11 01:30:57 -0600

    Database connects

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-console.log(process.env.DB_HOST)
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((err) => {
